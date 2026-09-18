ac674ceae4f462f60da54f95f76aaf131dc2f708
zainah23 <zainaho486@gmail.com>
2026-09-18 19:09:24 -0400

    feat: add periodic table program

---
+++ element.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit 0
+fi
+
+ELEMENT_INFO=$($PSQL "
+SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass,
+       p.melting_point_celsius, p.boiling_point_celsius, t.type
+FROM elements e
+JOIN properties p ON e.atomic_number = p.atomic_number
+JOIN types t ON p.type_id = t.type_id
+WHERE e.atomic_number::text = '$1'
+   OR e.symbol = '$1'
+   OR e.name = '$1';
+")
+
+if [[ -z $ELEMENT_INFO ]]
+then
+  echo "I could not find that element in the database."
+  exit 0
+fi
+
+IFS='|' read ATOMIC_NUMBER NAME SYMBOL MASS MELTING_POINT BOILING_POINT TYPE <<< "$ELEMENT_INFO"
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
